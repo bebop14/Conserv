e2472e143de553ced720032b3c91086568cff4f5
Yu SungDuk <sungduk.yu@navercorp.com>
2015-07-24 21:14:36 +0900

    config for docker-compose

--- config/index.js
+++ config/index.js
@@ -1,9 +1,9 @@
 module.exports = {
 	mongodb: {
-		url: 'mongodb://127.0.0.1:27017/conserv'
+		url: 'mongodb://mongo:27017/conserv'
 	},
 	redis: {
-		host: '127.0.0.1',
+		host: 'redis',
 		port: 6379
 	}
 };
